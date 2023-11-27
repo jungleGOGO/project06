a87390ef04de27f88be558986fc4504453427ef7
beubeu95 <xuanjiong810@gmail.com>
2023-11-27 17:40:53 +0900

    [231127 hyun] project test

---
+++ src/main/java/com/tcoding/team36/Team36Application.java
@@ -0,0 +1,13 @@
+package com.tcoding.team36;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team36Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team36Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/tcoding/team36/ctrl/MemoController.java
@@ -0,0 +1,32 @@
+package com.tcoding.team36.ctrl;
+
+import com.tcoding.team36.domain.Memo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+@Controller
+@Slf4j
+@RequestMapping("/api")
+public class MemoController {
+
+    @PostMapping("/test1")
+    @ResponseBody
+    public String setFile(@RequestBody Memo memo) throws Exception {
+        String filename = memo.getFilename();
+        String monaco = memo.getMonaco();
+        OutputStream file = new FileOutputStream("D:\\hk\\project\\file\\"+filename);
+        byte[] bt = monaco.getBytes(); //OutputStream은 바이트 단위로 저장됨
+        file.write(bt);
+        file.close();
+        log.info("filename : "+filename);
+        log.info("conetenz : "+monaco);
+        return filename;
+    }
+}
