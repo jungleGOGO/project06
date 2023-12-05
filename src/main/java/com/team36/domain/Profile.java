0bb28498667e3f20c351559ffd172a51b4aa67ab
beubeu95 <xuanjiong810@gmail.com>
2023-12-05 17:37:29 +0900

    [231205 hyun] 머지 시도

---
+++ src/main/java/com/team36/domain/Profile.java
@@ -0,0 +1,28 @@
+package com.team36.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Profile extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pno;
+    @Column(nullable = false)
+    private int mid;
+    @Builder.Default
+    private String memberImg = "basic.jpg";
+
+    private String intro;
+
+    private String gitLink1;
+
+    private String gitLink2;
+
+    private String gitLink3;
+}
